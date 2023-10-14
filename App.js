8f519dd30f7fec667a4eee02923b77410236f193
akashopp <106897340+akashopp@users.noreply.github.com>
2023-10-14 17:31:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Navbar from './Components/NavBar'
+import News from './Components/News'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar title='NewsMonkey'/>
+        <News/>
+      </div>
+    )
+  }
+}
